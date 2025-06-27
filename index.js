99cef50e72376fbe03c6b2d3b8ca19e0dee17ff9
Fujibird <johndeniel0@gmail.com>
2025-06-27 11:39:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Hi babii open this`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
